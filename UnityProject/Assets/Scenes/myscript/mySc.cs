b148252d237f3c6d1b996285cc76fc72102c6ada
fuhaixi <64693535+fuhaixi@users.noreply.github.com>
2020-12-18 15:27:12 +0800

    Update mySc.cs

--- UnityProject/Assets/Scenes/myscript/mySc.cs
+++ UnityProject/Assets/Scenes/myscript/mySc.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class mySc : MonoBehaviour
 {
     public string ss;
+    public Transform ts;
     // Start is called before the first frame update
     void Start()
     {
